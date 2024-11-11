12cfef5d3e92fb5ca3b648a0ab79a6acb12718af
Dimitris Petrou <petroudimitris@outlook.com>
2024-11-11 11:20:40 +0200

    More PyDoc updates

--- src/stelar_client/endpoints.py
+++ src/stelar_client/endpoints.py
@@ -7,6 +7,14 @@ class APIEndpointsV1:
     endpoints. This way, any modifications made here will be
     globally updated throughout the client's classes.
 
+    All endpoints contained here will have the api_url of the Client class
+    prepended when called through the request(...) method of the BaseAPI parent class.
+        
+        e.g.:
+        '/auth/token/issue'
+        will be configured to be used as 
+        http://mydomain.eu/stelar/api/v1/auth/token/issue
+
     Attributes:
         TOKEN_ISSUE (string): The endpoint for issuing an OAuth2.0 token
         TOKEN_VERIFY (string): The endpoint for verifying the status of an OAuth2.0 token
