405e7b90b689f9ad25423151c843e1f107557c49
Dimitris Petrou <petroudimitris@outlook.com>
2024-11-11 11:15:00 +0200

    Doc Updates

--- src/stelar_client/endpoints.py
+++ src/stelar_client/endpoints.py
@@ -5,7 +5,12 @@ class APIEndpointsV1:
     """
     This class contains a universal definition of the API
     endpoints. This way, any modifications made here will be
-    globally updated throughout the client's classes.    
+    globally updated throughout the client's classes.
+
+    Attributes:
+        TOKEN_ISSUE (string): The endpoint for issuing an OAuth2.0 token
+        TOKEN_VERIFY (string): The endpoint for verifying the status of an OAuth2.0 token
+        TOKEN_REFRESH (string): The endpoint for issuing an OAuth2.0 token based on a refresh token already available.
   
     """
     TOKEN_ISSUE = "/auth/token/issue"
