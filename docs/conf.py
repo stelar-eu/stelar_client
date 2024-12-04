a69784c7d3e4c4d4020b181b910286caf6970f30
Dimitris Petrou <petroudimitris@outlook.com>
2024-12-04 09:56:05 +0200

    Updated RTD settings for appearance

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "stelar_client"
-copyright = "2024, Vasilis Samoladas"
+copyright = "2024, STELAR-EU"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -153,7 +153,7 @@ todo_emit_warnings = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = "default"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
