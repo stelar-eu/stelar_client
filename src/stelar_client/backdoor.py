34d88f445abef6f68ec2e72acdb206e928ae4338
Vasilis Samoladas <vsamoladas@tuc.gr>
2024-12-20 16:35:03 +0200

    Added the backdoor code to access CKAN, useful for development.

--- src/stelar_client/backdoor.py
+++ src/stelar_client/backdoor.py
@@ -3,14 +3,22 @@
     This is useful for debugging and testing.
 """
 
+from stelar_client import Client
 from urllib.parse import urljoin
 import requests
+import pprint, json
+
+
+class ppdict(dict):
+    def __repr__(self):
+        #return pprint.saferepr(dict(self))
+        return json.dumps(self, indent=4)
 
 
 class CKAN:
-    def __init__(self, client):
-        self.client = client
-        self.ckanapi = urljoin(client._base_url, "/dc/api/3/action/")
+    def __init__(self):
+        self.client = Client('apitest')
+        self.ckanapi = urljoin(self.client._base_url, "/dc/api/3/action/")
         self.headers = {
             'Authorization': self.client._ckan_apitoken,
             #'Content-Type': 'application/json',
@@ -19,9 +27,14 @@ class CKAN:
     def doreq(self, verb, endp, params, data, headers={}):
         url = urljoin(self.ckanapi, endp)
         r = requests.request(verb, url, params=params, data=data, headers=self.headers|headers)
-        r.raise_for_status()
-        return r.json()
+        #r.raise_for_status()
+        #return ppdict(r.json())
+        return  r.json()
 
+    def __getattr__(self, name):
+        def ckan_call(data=None, **kwargs):
+            return self.POST(name, data=kwargs)
+        return ckan_call
 
     def GET(self, endp, params=None, headers={}, data={}):
         return self.doreq('GET', endp, params, data, headers)
